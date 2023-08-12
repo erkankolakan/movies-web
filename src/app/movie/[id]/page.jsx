50d4b3cb140281549230154a416038e1680a857e
Erkan <erkankolakan@gmail.com>
2023-08-12 16:15:18 +0300

    detay sayfası yapıldı

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 
     const getMovie = async(id) => {
@@ -21,10 +22,21 @@ const Page = async({params}) => {
   
 
   return (
-    <div>
-      Page
+    <div className='relative p-7 min-h-screen'>
+
+        <Image style={{objectFit:'cover'}} fill src={`https://image.tmdb.org/t/p/original/${movieDetail?.backdrop_path || movieDetail?.poster_path}`}  />
+        <div className='absolute'>
+            <div className='text-4xl font-bold my-3'> {movieDetail?.title} </div>
+            <div className='w-1/2'> {movieDetail?.overview} </div>
+            <div className='w-1/2'> {movieDetail?.overview} </div>
+            <div className='my-3'>{movieDetail?.release_date} - {movieDetail?.vote_average} </div>
+            <div className=' my-2 border w-32 p-2 rounded-md text-center text-lg cursor-pointer hover:bg-white hover:text-black transition-[2s]'> Trail</div>
+        </div>
+
     </div>
   )
 }
 
 export default Page
+
+4
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -9,10 +9,10 @@ const Movies = ({dt}) => {
 
   return (
     //resimlere özellik vermke için resmi nerden gönderdiiysek 
-    <div onClick={() => router.push(`/movie/${dt?.id}`)} className='w-[450px] relative cursor-pointer ' >
-        <Image width={450} height={300} src={`https://image.tmdb.org/t/p/original/${dt?.backdrop_path || dt?.poster_path}`}  />
+    <div style={{display:"inlineBlock"}} onClick={() => router.push(`/movie/${dt?.id}`)} className='w-[450px] overflow-hidden relative cursor-pointer ' >
+        <Image  style={{objectFit:'cover'}} width={450} height={300} src={`https://image.tmdb.org/t/p/original/${dt?.backdrop_path || dt?.poster_path}`}  />
 {/* ben bu özelliklerin resmin üzerine geldiğim zaman gözükmesini istiyorum, o yüzden aşağıdaki gibi yapıyorum */}
-        <div className='absolute bottom-0 p-2 w-full h-full flex flex-col justify-end opacity-0 hover:opacity-100 transition-opacity ' > 
+        <div className='erkan absolute bottom-0 p-2 w-full h-full flex flex-col justify-end opacity-0 hover:opacity-100 transition-opacity ' > 
            <div className='font-bold text-2xl'>{dt?.original_title}</div>
            <div>{dt?.release_date} - {dt?.vote_average} </div>
         </div>
